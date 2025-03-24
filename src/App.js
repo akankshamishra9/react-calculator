b2b48c8554a673046036fbf50e84eef52aa7e34a
Akanksha Mishra <94304465+akankshamishra9@users.noreply.github.com>
2025-03-24 18:06:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders React Calculator heading', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const headingElement = screen.getByText(/react calculator/i);
-  expect(headingElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
